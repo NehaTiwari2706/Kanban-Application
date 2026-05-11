7ed2413482b13232170a64d2e4268039dab542fe
kanban appication ogin register page <tiwari27neha06@gmail.com>
2026-05-11 23:12:06 +0530

    added dtolombok  annotations

--- project/src/main/java/com/example/project/dto/IterationDTO.java
+++ project/src/main/java/com/example/project/dto/IterationDTO.java
@@ -3,51 +3,33 @@ package com.example.project.dto;
 import java.sql.Time;
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class IterationDTO {
     
     private Long id;
+    
+    @NotBlank
     private String iterationnumber;
+    
+    @NotBlank
     private String name;
+    
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate startDate;
+    
+    @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate endDate;
+    
     private String status;
     private Long teamId;
     private Time created_at;
-
-    public IterationDTO() {}
-
-    public IterationDTO(Long id, String iterationnumber, String name, LocalDate startDate, LocalDate endDate, String status, Long teamId, Time created_at) {
-        this.id = id;
-        this.iterationnumber = iterationnumber;
-        this.name = name;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.status = status;
-        this.teamId = teamId;
-        this.created_at = created_at;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getIterationNumber() { return iterationnumber; }
-    public void setIterationNumber(String iterationnumber) { this.iterationnumber = iterationnumber; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-
-    public LocalDate getStartDate() { return startDate; }
-    public void setStartDate(LocalDate startDate) { this.startDate = startDate; }
-
-    public LocalDate getEndDate() { return endDate; }
-    public void setEndDate(LocalDate endDate) { this.endDate = endDate; }
-
-    public String getStatus() { return status; }
-    public void setStatus(String status) { this.status = status; }
-
-    public Long getTeamId() { return teamId; }
-    public void setTeamId(Long teamId) { this.teamId = teamId; }
-
-    public Time getCreated_at() { return created_at; }
-    public void setCreated_at(Time created_at) { this.created_at = created_at; }
 }
